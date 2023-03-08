5370f4df1bdb5907c337503cf76f3f6cdfab440a
Gabriel Sasaki <gabrielpintosasaki@outlook.com>
2023-03-08 16:38:45 -0300

    feat: edit and delete button

--- src/app/components/product/product-read/product-read.component.ts
+++ src/app/components/product/product-read/product-read.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductReadComponent implements OnInit {
   products: Product[] = []
-  displayedColumns: string[] = ['id', 'name', 'price']
+  displayedColumns: string[] = ['id', 'name', 'price', 'action']
 
   constructor(private productService: ProductService) {
 
