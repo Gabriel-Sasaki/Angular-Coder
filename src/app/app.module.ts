1ea531054d797386b983e4f4858f39ec1a48a651
Gabriel Sasaki <gabrielpintosasaki@outlook.com>
2023-03-08 16:25:15 -0300

    feat: read table products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -22,8 +22,12 @@ import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input';
-import { ProductReadComponent } from './components/product/product-read/product-read.component'
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { MatTableModule } from '@angular/material/table';
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common'
 
+registerLocaleData(localePt)
 
 @NgModule({
   declarations: [
@@ -34,7 +38,7 @@ import { ProductReadComponent } from './components/product/product-read/product-
     HomeComponent,
     ProductCrudComponent,
     ProductCreateComponent,
-    ProductReadComponent
+    ProductReadComponent,
   ],
   imports: [
     BrowserModule,
@@ -49,9 +53,13 @@ import { ProductReadComponent } from './components/product/product-read/product-
     HttpClientModule,
     FormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTableModule,
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/product/product-read/product-read.component.ts
+++ src/app/components/product/product-read/product-read.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { ProductService } from './../product.service';
+import { Product } from './../product.model';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-read',
   templateUrl: './product-read.component.html',
   styleUrls: ['./product-read.component.css']
 })
-export class ProductReadComponent {
+export class ProductReadComponent implements OnInit {
+  products: Product[] = []
+  displayedColumns: string[] = ['id', 'name', 'price']
 
+  constructor(private productService: ProductService) {
+
+  }
+
+  ngOnInit(): void {
+    this.productService.read().subscribe(products => {
+      this.products = products
+    })
+  }
 }
